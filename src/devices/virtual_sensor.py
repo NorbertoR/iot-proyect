0bfe8811e864e0c9e64bfb5bd5da257a0ce6bcd5
NorbertoR <norberto1976@hotmail.com>
2025-09-13 13:13:47 +0000

    Actualizo mqtt.Client

--- src/devices/virtual_sensor.py
+++ src/devices/virtual_sensor.py
@@ -2,31 +2,16 @@ import time
 import random
 import json
 import paho.mqtt.client as mqtt
+from paho.mqtt.client import CallbackAPIVersion
+from paho.mqtt.client import CallbackAPIVersion
 
 BROKER = "mosquitto"  # nombre del servicio en docker-compose
 PORT = 1883
 TOPIC = "sensors/ambient"
-RETRIES = 10
+RETRIES = 10 # número de reintentos
 DELAY = 3 # segundos
 
-client = mqtt.Client()
-#client.connect(BROKER, PORT, 60)
-#
-#try:
-#    while True:
-#        data = {
-#           "temperature": round(random.uniform(18, 30), 2),
-#           "humidity": round(random.uniform(40, 70), 2),
-#            "timestamp": time.time()
-#        }
-#        payload = json.dumps(data)
-#        client.publish(TOPIC, payload)
-#        print(f"Publicado: {payload}")
-#        time.sleep(5)
-#except KeyboardInterrupt:
-#    print("Sensor detenido.")
-#finally:
-#    client.disconnect()
+client = mqtt.Client(protocol=mqtt.MQTTv311)
 
 for attempt in range(RETRIES):
     try:
